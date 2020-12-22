250338f7fdd2b773d672cf312708032a83b4d42e
Souravsinha96 <souravsinha1604@gmail.com>
2020-12-22 10:26:13 +0530

    Adding a list of videos from Youtube API

---
+++ src/apis/youtube.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    type: "video",
+    key: process.env.REACT_APP_API_KEY,
+  },
+});
